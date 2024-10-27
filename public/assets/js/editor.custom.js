589ba2abd0b54b1c8c74d73ac4e72188eb151be6
OkeleyeVickkk <justusofnigeria@gmail.com>
2024-10-27 14:05:40 +0100

    new day, new commits

--- public/assets/js/editor.custom.js
+++ public/assets/js/editor.custom.js
@@ -1,4 +1,4 @@
-import { callDomEle, setLoadStatus } from "./utils.custom.js";
+import { callDomEle, setLoadStatus, showToast, BASE_URL } from "./utils.custom.js";
 
 (() => {
 	let editor = null;
@@ -8,12 +8,63 @@ import { callDomEle, setLoadStatus } from "./utils.custom.js";
 		if (!parentBlogContainer) return;
 		const publishBlogToggler = callDomEle(".v-publish-blog-toggler", parentBlogContainer);
 		if (!publishBlogToggler) return;
-		publishBlogToggler.addEventListener("click", function () {
+		publishBlogToggler.addEventListener("click", async function () {
 			const target = this;
 			setLoadStatus(target, undefined, true);
 
 			if (editor !== null) {
-				console.log(editor.getHTMLCode());
+				const editorContent = editor.getHTMLCode();
+				const blogTitle = callDomEle(".v-main-title", parentBlogContainer);
+				const blogSubtitle = callDomEle(".v-subtext", parentBlogContainer);
+
+				const blogElementEntity = {
+					editorContent,
+					blogTitle,
+					blogSubtitle,
+				};
+
+				console.log(editorContent);
+				return;
+
+				for (const data in blogElementEntity) {
+					if (data === "blogSubtitle") continue;
+					if (blogElementEntity[data] instanceof Node) {
+						if (blogElementEntity[data].value.trim() === "") {
+							const attr = blogElementEntity[data].getAttribute("name");
+							let errorValue;
+							if (!attr) return;
+							if (attr.toLowerCase() === "blogtitle") {
+								errorValue = "Blog title is empty!";
+							}
+							setLoadStatus(target, "Publish");
+							return showToast(errorValue, "failed");
+						}
+					} else if (typeof blogElementEntity[data] === "string") {
+						if (blogElementEntity[data]?.trim() === "") {
+							showToast("There is no data in your blog write-up", "error");
+							return setLoadStatus(target, "Publish");
+						}
+					} else {
+						alert("Error occurred");
+					}
+				}
+
+				const blogDataValue = {
+					blogTitle: blogTitle.value,
+					blogSubtitle: blogSubtitle.value,
+					blogContent: editorContent.value,
+				};
+				const baseUrl = `${BASE_URL}/dashboard/write`;
+
+				const response = await fetch(baseUrl, {
+					method: "POST",
+					headers: {
+						Accept: "application/json",
+						"x-custom-update": "postBlog",
+					},
+					body: JSON.stringify(blogDataValue),
+				});
+				const result = await response.json();
 			}
 		});
 	};
